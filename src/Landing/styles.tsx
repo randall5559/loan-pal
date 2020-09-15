6c4e1400ca032757cebdd80ae6f8fc496d0acd52
chris Akers <chrisakers@chriss-Air.home>
2020-09-14 22:44:37 -0400

    • Added header with logo • Added login, apply and status button • Minor refactoring

---
+++ src/Landing/styles.tsx
@@ -0,0 +1,12 @@
+import { makeStyles, Theme } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme: Theme) => ({
+    button: {
+        backgroundColor: '#128A98',
+        margin: '10px 0',
+        padding: 15,
+        minWidth: 300,
+        maxWidth: 350,
+        alignSelf: 'center'
+    },
+}));
