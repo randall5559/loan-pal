74c3205831b19fecfdb240dd0eca7cdede40200b
chris Akers <chrisakers@chriss-Air.home>
2020-09-16 16:38:11 -0400

    • Added experimentalDecorators to tsconfig.json • Installed firebase and setup firebase configuration • Created a akita entity for users

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
  
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,19 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Initialize firebase realtime database
+const firebaseConfig = {
+  apiKey: "AIzaSyDNKjvFTmVhWvMJKf6wbVYwIO5_35cfzYw",
+  authDomain: "loan-pal-7f2ab.firebaseapp.com",
+  databaseURL: "https://loan-pal-7f2ab.firebaseio.com",
+  projectId: "loan-pal-7f2ab",
+  storageBucket: "loan-pal-7f2ab.appspot.com",
+  messagingSenderId: "473824805041",
+  appId: "1:473824805041:web:fa144eab5cbcd8fc3f2290",
+  measurementId: "G-NHF1YE3DN6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
